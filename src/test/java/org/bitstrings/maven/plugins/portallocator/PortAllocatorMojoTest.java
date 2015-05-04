e30a96e6e6e16d7eae97fe5b4d32bf5c4931a995
Pino Silvaggio <pino.silvaggio@gmail.com>
2015-05-04 16:01:02 -0400

    start setting up tests

---
+++ src/test/java/org/bitstrings/maven/plugins/portallocator/PortAllocatorMojoTest.java
@@ -0,0 +1,29 @@
+package org.bitstrings.maven.plugins.portallocator;
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class PortAllocatorMojoTest
+{
+    @Rule
+    public MojoRule rule = new MojoRule()
+    {
+      @Override
+      protected void before()
+          throws Throwable
+      {
+      }
+
+      @Override
+      protected void after()
+      {
+      }
+    };
+
+    @Test
+    public void simplest()
+    {
+
+    }
+}
