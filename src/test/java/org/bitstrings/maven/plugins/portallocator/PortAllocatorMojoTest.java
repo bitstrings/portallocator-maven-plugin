954645bb925a7e578e4f25e4d453cae60289062d
Pino Silvaggio <pino.silvaggio@gmail.com>
2015-05-04 17:11:49 -0400

    setting up tests

--- src/test/java/org/bitstrings/maven/plugins/portallocator/PortAllocatorMojoTest.java
+++ src/test/java/org/bitstrings/maven/plugins/portallocator/PortAllocatorMojoTest.java
@@ -1,29 +1,40 @@
 package org.bitstrings.maven.plugins.portallocator;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+
 import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.resources.TestResources;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class PortAllocatorMojoTest
 {
     @Rule
-    public MojoRule rule = new MojoRule()
-    {
-      @Override
-      protected void before()
-          throws Throwable
-      {
-      }
+    public MojoRule rule = new MojoRule();
 
-      @Override
-      protected void after()
-      {
-      }
-    };
+    @Rule
+    public TestResources resources = new TestResources();
 
     @Test
     public void simplest()
+        throws Exception
     {
+        File pom = getTestFile( "src/test/resources/unit/project-to-test/pom.xml" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
 
+        PortAllocatorMojo myMojo = (PortAllocatorMojo) rule.lookupMojo( "allocate", pom );
+        assertNotNull( myMojo );
+        myMojo.execute();
+    }
+
+    private File getTestFile( String testProjectPath )
+        throws Exception
+    {
+        resources.getBasedir( "test--" );
+
+        return null;
     }
 }
