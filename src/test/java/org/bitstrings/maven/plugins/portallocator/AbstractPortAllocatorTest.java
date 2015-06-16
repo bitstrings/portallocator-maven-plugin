2ce38dc83af25ff0a225c58ec6db4aaaf75ea908
Pino Silvaggio <pino.silvaggio@gmail.com>
2015-06-16 14:51:51 -0400

    remove @Before - resetting mojo static fields

--- src/test/java/org/bitstrings/maven/plugins/portallocator/AbstractPortAllocatorTest.java
+++ src/test/java/org/bitstrings/maven/plugins/portallocator/AbstractPortAllocatorTest.java
@@ -3,7 +3,6 @@ package org.bitstrings.maven.plugins.portallocator;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.resources.TestResources;
 import org.apache.maven.project.MavenProject;
-import org.junit.Before;
 import org.junit.Rule;
 
 public class AbstractPortAllocatorTest
@@ -18,20 +17,5 @@ public class AbstractPortAllocatorTest
         throws Exception
     {
         return rule.readMavenProject( resources.getBasedir( "base" ) );
-    }
-
-    @Before
-    public void setUp()
-        throws Exception
-    {
-        // yeah I know it's f*ckn ugly
-//        ( (Set<?>) FieldUtils.readStaticField(
-//                PortAllocatorMojo.class,
-//                "ALLOCATED_PORTS",
-//                true )
-//        ).clear();
-
-        //new ServerSocketMock();
-        //new PortAllocatorServiceMock();
     }
 }
